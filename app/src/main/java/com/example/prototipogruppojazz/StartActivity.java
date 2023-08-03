cea1402963ef75934b77eb9be5d8a523ac08c7a3
Rasfesk <Matteomelis1998@gmail.com>
2023-08-03 19:47:16 +0200

    primo commit

---
+++ app/src/main/java/com/example/prototipogruppojazz/StartActivity.java
@@ -0,0 +1,39 @@
+package com.example.prototipogruppojazz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+    Button accedi,registrati;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        accedi=findViewById(R.id.buttonLogin);
+        registrati=findViewById(R.id.buttonSignin);
+
+        accedi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(StartActivity.this, LoginActivity.class);
+
+                startActivity(login);
+            }
+        });
+
+        registrati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signin = new Intent(StartActivity.this, SigninActivity.class);
+
+                startActivity(signin);
+            }
+        });
+
+    }
+}
